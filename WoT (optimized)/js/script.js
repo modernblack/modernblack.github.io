ca66abe0760088a2ea76a6ebfea6b123cd897e93
modernblack <83514007+modernblack@users.noreply.github.com>
2021-05-15 22:26:10 +0300

    update

--- WoT (optimized)/js/script.js
+++ WoT (optimized)/js/script.js
@@ -1,6 +1,19 @@
+var w = screen.width;
 $(document).ready(function() {
 	$('.header__burger').click(function(event) {
 		$('.header__burger,.header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+});
+function forMobile() {
+	w = screen.width;
+	if (w <= 425) {
+		$('.link_list').contents().unwrap();
+		$('.img_list').contents().unwrap();
+	}
+}
+forMobile();
+
+$( window ).resize(function() {
+    forMobile();
 });
